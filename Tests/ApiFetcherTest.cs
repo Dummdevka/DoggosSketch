108eaf937dc949fa6ae438c966662a0237473649
Dummdevka <87282400+Dummdevka@users.noreply.github.com>
2023-08-13 12:35:56 +0300

    added tests for ApiFetcher

--- Tests/ApiFetcherTest.cs
+++ Tests/ApiFetcherTest.cs
@@ -10,16 +10,41 @@ namespace Tests
 			get; set;
 		}
 
-		public ApiFetcherTest(IConfiguration config)
+		public IConfiguration _config;
+
+		public ApiFetcherTest()
 		{
-			_apiFetcher = new ApiFetcher(config);
+			Dictionary<string, string> configuration = new() {
+				{ "ApiUrl", "https://dog.ceo/api" }
+			};
+			_config = new ConfigurationBuilder()
+				.AddInMemoryCollection(configuration)
+				.Build();
+			_apiFetcher = new ApiFetcher(_config);
 		}
 
 		[Fact]
-		public void FetchBreeds_ValidCall() {
-			var actual = _apiFetcher.FetchBreeds();
+		public async Task FetchBreeds_ValidCall() {
+			List<string> actual = await _apiFetcher.FetchBreeds();
 
-			Assert.True(actual is IList<string>);
+			Assert.True(actual is not null);
+			Assert.True(actual is IEnumerable<string>);
+		}
+
+		[Fact]
+		public async Task FetchRandomImages_ValidCall() {
+			List<string> actual = await _apiFetcher.FetchRandomImages();
+
+			Assert.True(actual is not null);
+			Assert.True(actual is IEnumerable<string>);
+		}
+
+		[Fact]
+		public async Task FetchImagesByBreed_ValidCall() {
+			List<string> actual = await _apiFetcher.FetchImagesByBreed("collie");
+
+			Assert.True(actual is not null);
+			Assert.True(actual is IEnumerable<string>);
 		}
 	}
 }
